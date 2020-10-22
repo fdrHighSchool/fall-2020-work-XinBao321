de169a8087f2c1c10a3a263b0f1d5e0904f9a7ec
XinBao321 <xinbaoc2@nycstudents.net>
2020-10-22 11:00:15 -0400

    comments and small fix

--- 04-shapes/Shapes.java
+++ 04-shapes/Shapes.java
@@ -36,7 +36,7 @@ public static double triangle(double base, double height){
   System.out.println("What is the height of the triangle ?");
   height  = input.nextDouble();
   double areaTri = base * height /2;
-  return areaTri;
+  return areaTri; // return statement
 }//close method for triangle
 
 public static double herons(double length1,double length2, double length3){
@@ -49,7 +49,7 @@ public static double herons(double length1,double length2, double length3){
   length3 = input.nextDouble();
   double cool =(length1 + length2 + length3)/2;
   double area = Math.sqrt(cool*(cool - length1)*(cool - length2)*(cool - length3));
-  return area;
+  return area; // return statement
 }//end of heron's method
 
 public static double circle(double radius, double area){
@@ -57,7 +57,7 @@ public static double circle(double radius, double area){
  System.out.println("What is the radius of the circle?");
  radius = input.nextDouble();
  area = Math.PI * radius * radius;
- return area;
+ return area;  // return statement
 } // close method for circle
 
   public static double cube(double s, double areaCube){
@@ -65,7 +65,7 @@ public static double circle(double radius, double area){
     System.out.println("Enter the side of the cube ");
     s = input.nextDouble();
     areaCube = Math.pow(s,3);
-    return areaCube;
+    return areaCube;  // return statement
   } //close method for cube
 
   public static double triangularPrism(double length3, double base, double height, double volumeTri, double valueCheck ){
@@ -77,7 +77,7 @@ public static double circle(double radius, double area){
     System.out.println("What is the height of the triangleprism? ");
     height = input.nextDouble();
     volumeTri = length3 * base * height /2;
-    return volumeTri;
+    return volumeTri;  // return statement
   }// close method for triangularprism
 
   public static double rectangularprism(double length4, double width3, double height2, double volumeRect){
@@ -89,7 +89,7 @@ public static double circle(double radius, double area){
     System.out.println("What is the height of the rectangular prism?");
     height2 = input.nextDouble();
     volumeRect = length4 * width3 * height2;
-    return volumeRect;
+    return volumeRect;  // return statement
   }  //close method for rectangularprism
 
     public static double sphere(double radius, double volumeSphere,double volumeSphere1){
@@ -98,7 +98,7 @@ public static double circle(double radius, double area){
       radius = input.nextDouble();
       volumeSphere1 = Math.PI * radius * radius * radius;
       volumeSphere = volumeSphere1 * 4/3;
-      return volumeSphere;
+      return volumeSphere;  // return statement
     } // close method for sphere
 
     public static double cylinder(double radius, double h, double volumeCylinder){
@@ -108,7 +108,7 @@ public static double circle(double radius, double area){
       System.out.println("What is the height of the cylinder?");
       h = input.nextDouble();
       volumeCylinder = (radius * radius * h * Math.PI);
-      return volumeCylinder;
+      return volumeCylinder;  // return statement
     }// close method for cylinder
 
  }//close class
