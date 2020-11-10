20419562e50dda573fa14e3349703e4b33f61d48
XinBao321 <xinbaoc2@nycstudents.net>
2020-11-09 22:21:34 -0500

    created multication method changed string into ints and debugg some code

--- 08-calculator_project/FracCalc.java
+++ 08-calculator_project/FracCalc.java
@@ -15,7 +15,7 @@ public class Main{
       System.out.println("Or type the word quit to leave! :)");//options
       String userInput = user.nextLine(); //store usersinput
       if (userInput.equalsIgnoreCase("quit")){//whole loop until user type quit
-      continues = false; //if user type quit the while loop stops
+        continues = false; //if user type quit the while loop stops
         }else{ //otherwise print out answer
         System.out.println(produceAnswer(userInput));//results
         } //end else statement
@@ -39,11 +39,10 @@ public class Main{
      * @return the result of the fraction after it has been calculated.
      *      Example: return ==> "1_1/4"
      */
-
-     // Name: produceAnswer()
-     // Purpose: identify the operator and fractions of user inputs and printing out the results
-     // Input: input of expression from user
-     // Return: return the numerator, denominator, and whole as checkpoint2(results)
+  // Name: produceAnswer()
+  // Purpose: identify the operator and fractions of user inputs and printing out the results
+  // Input: input of expression from user
+  // Return: return the numerator, denominator, and whole as checkpoint2(results)
     public static String produceAnswer(String input){
       int space = input.indexOf(" "); //when there is a space
       int lastIndex = input.length(); //from the last occurrence of the whole equation
@@ -52,73 +51,136 @@ public class Main{
       String operand2 = input.substring(space + 3, lastIndex); //starting after the third space and everything after that is included
       //return operand2;
       //checkpoint 1 complete
-      String numerator1 = findNumerator(operand1);//get first numerator
-      String numerator2 = findNumerator(operand2);//get second numerator
-      String Whole1 = findWhole(operand1); //get first whole
-      String Whole2 = findWhole(operand2); //get second whole
-      String denominator1 = findDenominator(operand1); //get first denominator
-      String denominator2 = findDenominator(operand2); //get second denominator
+      int numerator1 = findNumerator(operand1);//get first numerator
+      int denominator1 = findDenominator(operand1);;//get first denominator
+      int Whole1 = findWhole(operand1); //get first whole
+      int Whole2 = findWhole(operand2); //get second whole
+      int numerator2 = findNumerator(operand2);//get second numerator
+      int denominator2 = findDenominator(operand2);//get second denominator
       String checkPoint2 = "Whole:" + Whole1 +"/ " + Whole2 + " Numerator:" + numerator1 + "/ " + numerator2 +  " Denominator:" + denominator1 + "/ " + denominator2;
       //printin out results
       //System.out.println(Whole1, Numerator1, denominator1,Whole2,numerator2,denominator2 );
-      return checkPoint2; //checkpoint 2 complete
-      //All of the contains() method checks whether a string contains a sequence of characters
-    } //end of produceAnswer method
+      if(operator.contains("*")){
+        System.out.println("The results is: " + multiply(numerator1, numerator2,  Whole1,  Whole2,  denominator1, denominator2));
+        return checkPoint2;//checkpoint 2 complete
+      }else return "0";
+    }//end of produceAnswer method
 
-    // Name: findWhole()
-    // Purpose: To look for whole numbers inside a fraction
-    // Input: Two fractions strings from the user
-    // Return: If the fractions contains whole number and return results back to produceAnswer()
-   public static String findWhole(String find){
+  //All of the contains() method checks whether a string contains a sequence of characters
+  // Name: findWhole()
+  // Purpose: To look for whole numbers inside a fraction
+  // Input: Two fractions strings from the user
+  // Return: If the fractions contains whole number and return results back to produceAnswer()
+   public static int findWhole(String find){
       // 4421_1/3    1234/12     5821
       if(find.contains("_")){ //if there is a "_" sign it means it is a mixed number
-       return find.substring(0, find.indexOf("_"));
+        return Integer.parseInt(find.substring(0, find.indexOf("_")));
        //return everything from beginning until seeing “_”
     } //end if statement
       if(find.contains("/")){
-       return "0";  //if it is a fraction return 0 as whole number
-    } //end of if statement
-      else return find;
+       return 0;  //if it is a fraction return 0 as whole number
+    } //end if statement
+      else return Integer.parseInt(find);
      //if there is no fraction sign or mixed number, everything else is whole number
     }//end of findWhole method
 
-   // Name: findNumerator()
-   // Purpose: To look for numerator inside a fraction
-   // Input: Two fractions strings from the user
-   // Return: If the fractions contains numerator and return results back to produceAnswer()
-   public static String findNumerator(String find){
+  // Name: findNumerator()
+  // Purpose: To look for numerator inside a fraction
+  // Input: Two fractions strings from the user
+  // Return: If the fractions contains numerator and return results back to produceAnswer()
+    public static int findNumerator(String find){
       //3_5/8   233/1   42821
       if(find.contains("_")){  //if it is a mixed number
-        return find.substring(find.indexOf("_") + 1,find.indexOf("/"));
+        return Integer.parseInt(find.substring(find.indexOf("_") + 1,find.indexOf("/")));
         //add one because there is a space between two the operator and the sign
         //numerator is everything in front of the "_" sign
        }else if(find.contains("/")){
-        return find.substring(0, find.indexOf("/")); //get everything before the "/"
+        return Integer.parseInt(find.substring(0, find.indexOf("/"))); //get everything before the "/"
         }else{
-        return "0"; //if there is no fraction or mixed number return 0 as numerator
+        return 0; //if there is no fraction or mixed number return 0 as numerator
      } //end of else statement
     }//end of findNumerator method
 
-   // Name: findDenominator
-   // Purpose: To look for denomator inside a fraction
-   // Input: Two fractions strings from the user
-   // Return: If the fractions contains denominator and return results back to produceAnswer()
-   public static String findDenominator(String find){
+  // Name: findDenominator
+  // Purpose: To look for denomator inside a fraction
+  // Input: Two fractions strings from the user
+  // Return: If the fractions contains denominator and return results back to produceAnswer()
+    public static int findDenominator(String find){
       //1_7/2   3125/42   41236
       if(find.contains("/")){
-        return find.substring(find.indexOf("/") + 1);//the denomator is after the "/" sign
+      return Integer.parseInt(find.substring(find.indexOf("/") + 1));//the denomator is after the "/"
         //add one to get rid of the "/" sign
         //get everything after the "/"
         }else{
-        return "1"; //You can't have zero as an denominator!
+        return 1; //You can't have zero as an denominator!
         }//end of else statement
     }//end of findDenominator method
+  // Name:
+  // Purpose:
+  // Input:
+  // Return
+    public static String multiply(int numerator1, int numerator2, int Whole1, int Whole2, int denominator1, int denominator2){
+     if(Whole1 == 0 || Whole2 == 0){
+      int firstNumerator = numerator1 * numerator2;
+      int firstDenominator = denominator1 * denominator2;
+      String answer = firstNumerator + "/" + firstDenominator;
+      return answer;
+    }else
+      if(Whole1 != 0 || Whole2 != 0){
+        int removeWhole1 = (denominator1 * Whole1) + numerator1;
+        int removeWhole2 = (denominator2 * Whole2) + numerator2;
+        int finalNumerator1 = removeWhole1 * removeWhole2;
+        int finalDenominator = denominator1 * denominator2;
+        String answer = finalNumerator1 + "/" + finalDenominator;
+        return answer;
+    }else
+      /*if(Whole1 > 0 || Whole2 > 0){
+        int removeWhole1 = denominator1
+      }
+      else return "0";
+    }
+    }//end class
 
 
-   public static String mixedFraction(String find){
-      return "0";
-    } //end mixedFraction method
-  }//end class
+        // TODO: Implement this function to produce the solution to the input
+        // Checkpoint 1: Return the second operand.  Example "4/5 * 1_2/4" returns "1_2/4".
+        // Checkpoint 2: Return the second operand as a string representing each part.
+        //               Example "4/5 * 1_2/4" returns "whole:1 numerator:2 denominator:4".
+        // Checkpoint 3: Evaluate the formula and return the result as a fraction.
+        //               Example "4/5 * 1_2/4" returns "6/5".
+        //               Note: Answer does not need to be reduced, but it must be correct.
+        // Final project: All answers must be reduced.
+        //               Example "4/5 * 1_2/4" returns "1_1/5".
+
+
+    // TODO: Fill in the space below with helper methods
+
+    /**
+     * greatestCommonDivisor - Find the largest integer that evenly divides two integers.
+     *      Use this helper method in the Final Checkpoint to reduce fractions.
+     * @param a - First integer.
+     * @param b - Second integer.
+     * @return The GCD.
+     */
+    /**public static int greatestCommonDivisor(int a, int b){
+
+      return 0;
+    }//end greatestCommonDivisor method
+
+    /**
+     * leastCommonMultiple - Find the smallest integer that can be evenly divided by two integers.
+     *      Use this helper method in Checkpoint 3 to evaluate expressions.
+     * @param a - First integer.
+     * @param b - Second integer.
+     * @return The LCM.
+     */
+    /**public static int leastCommonMultiple(int a, int b){
+
+      //return 0;
+    }//end leastCommonMultiple
+    */
+
+//end class
 
 
         // TODO: Implement this function to produce the solution to the input
