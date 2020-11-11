ca09339efa974bc02ac37ad7d10ce32ff1c46702
XinBao321 <xinbaoc2@nycstudents.net>
2020-11-11 18:09:18 -0500

    Fixed repetitions and added additions

--- 08-calculator_project/FracCalc.java
+++ 08-calculator_project/FracCalc.java
@@ -23,10 +23,6 @@ public class Main{
     }// end main
 
 
-
-    //end of while loop
-
-
         // TODO:Read the input from the user and call produceAnswer with an equation
         // Checkpoint 1: Create a Scanner, read one line of input, pass that input to produceAnswer, print the result.
         // Checkpoint 2: Accept user input multiple times.
@@ -57,16 +53,28 @@ public class Main{
       int Whole2 = findWhole(operand2); //get second whole
       int numerator2 = findNumerator(operand2);//get second numerator
       int denominator2 = findDenominator(operand2);//get second denominator
-      String checkPoint2 = "Whole:" + Whole1 +"/ " + Whole2 + " Numerator:" + numerator1 + "/ " + numerator2 +  " Denominator:" + denominator1 + "/ " + denominator2;
+      String checkPoint2 = "Whole:" + Whole2  + " Numerator:" + numerator2 +  " Denominator:" +  denominator2;
       //printin out results
       //System.out.println(Whole1, Numerator1, denominator1,Whole2,numerator2,denominator2 );
+
+      //Looking for operations
       if(operator.contains("*")){
-        System.out.println("The results is: " + multiply(numerator1, numerator2,  Whole1,  Whole2,  denominator1, denominator2));
-        return checkPoint2;//checkpoint 2 complete
-      }else return "0";
+        System.out.println("The multiply results are: " + multiply(numerator1, numerator2,  Whole1,  Whole2,  denominator1, denominator2, improperFraction(numerator1,Whole1, denominator1), improperFraction(numerator2, Whole2, denominator2)));
+      } //end of "*" if statment
+
+      else if(operator.contains("+") && denominator1 != denominator2){
+      System.out.println("The addition results are: " + additionDifferentNumerator(numerator1,numerator2,Whole1, Whole2,denominator1,denominator2, improperFraction(numerator1,Whole1,denominator1), improperFraction(numerator2, Whole2, denominator2)));
+      }//end of + if statement
+
+      else if(operator.contains("+") && denominator1 == denominator2){
+        System.out.println("The addition results are: " + additionSameNumerator(numerator1, numerator2, Whole1, Whole2, denominator1, denominator2, improperFraction(numerator1, Whole1, denominator1), improperFraction(numerator2, Whole2, denominator2)));
+
+      }//end of + if statement
+      else return "0";
+      return checkPoint2;//checkpoint 2 complete
     }//end of produceAnswer method
 
-  //All of the contains() method checks whether a string contains a sequence of characters
+  // All of the contains() method checks whether a string contains a sequence of characters
   // Name: findWhole()
   // Purpose: To look for whole numbers inside a fraction
   // Input: Two fractions strings from the user
@@ -115,72 +123,122 @@ public class Main{
         return 1; //You can't have zero as an denominator!
         }//end of else statement
     }//end of findDenominator method
+
   // Name:
   // Purpose:
   // Input:
-  // Return
-    public static String multiply(int numerator1, int numerator2, int Whole1, int Whole2, int denominator1, int denominator2){
-     if(Whole1 == 0 || Whole2 == 0){
+  // Return:
+    public static String multiply(int numerator1, int numerator2, int Whole1, int Whole2, int denominator1, int denominator2, int removeWhole1, int removeWhole2){
+      //multiply the two fractions
+     if(Whole1 == 0 && Whole2 == 0){ //if there is no whole numbers
+     //just multiply fraction because there is no whole number in this case
       int firstNumerator = numerator1 * numerator2;
       int firstDenominator = denominator1 * denominator2;
       String answer = firstNumerator + "/" + firstDenominator;
       return answer;
     }else
-      if(Whole1 != 0 || Whole2 != 0){
-        int removeWhole1 = (denominator1 * Whole1) + numerator1;
-        int removeWhole2 = (denominator2 * Whole2) + numerator2;
+        if(Whole1 > 0 && Whole2 > 0){ //most of time there are whole numbers
+        //remove the whole numbers
+        //You times them after in improper fraction
         int finalNumerator1 = removeWhole1 * removeWhole2;
-        int finalDenominator = denominator1 * denominator2;
+        int finalDenominator = denominator1 * denominator2;//simpily multiply them
         String answer = finalNumerator1 + "/" + finalDenominator;
         return answer;
     }else
-      /*if(Whole1 > 0 || Whole2 > 0){
-        int removeWhole1 = denominator1
-      }
+        if(Whole1 > 0 && Whole2 == 0){ //when fraction1 has a whole numbers but fraction2 doesn't
+        //You times them after in improper fraction
+        int answerNumerator1 = removeWhole1 * numerator1;
+        int answerDenominator = denominator1 * denominator2;
+        String answer = answerNumerator1 + "/" + answerDenominator;
+        return answer;
+      }else
+        if(Whole2 > 0 && Whole1 == 0){
+        //when fraction2 has a whole numbers but fraction1 doesn't
+        //You times them after in improper fraction
+        int answerNumerator2 = removeWhole2 * numerator2;
+        int answerDenominator = denominator1 * denominator2;
+        String answer = answerNumerator2 + "/" + answerDenominator;
+        return answer;
+    }//end if statement
       else return "0";
-    }
-    }//end class
+  }//end of multiply method
+
+    public static String additionDifferentNumerator(int numerator1, int numerator2, int Whole1, int Whole2, int denominator1, int denominator2, int removeWhole1, int removeWhole2){
+      //if the denominator are not the same
+      if(Whole1 > 0 && Whole2 > 0){//greater than zero
+      //System.out.println(removeWhole1);
+      //System.out.println(removeWhole2);
+      int firstNumerator = (removeWhole1 * denominator2) + (removeWhole2 * denominator1); //removeWhole2 is the improperFraction of fraction2
+      int firstDenominator = denominator1 * denominator2;
+      String answer = firstNumerator + "/" + firstDenominator;
+      return answer;
+    }else
+      if(Whole1 == 0 && Whole2 == 0){ //if there is no whole numbers
+      //just multiply fraction because there is no whole number in this case
+      int firstNumerator = (numerator1 * denominator2) + (denominator2 * numerator2);
+      int firstDenominator = denominator1 * denominator2;
+      String answer = firstNumerator + "/" + firstDenominator;
+      return answer;
+    }else
+     if(Whole2 > 0 && Whole1 == 0){
+     //when fraction2 has a whole numbers but fraction1 doesn't
+     //add them after in improper fraction
+       int firstNumerator = (numerator1 * denominator2) + (removeWhole2 * denominator1); //removeWhole2 is the improperFraction of fraction2
+       int firstDenominator = denominator1 * denominator2;
+       String answer = firstNumerator + "/" + firstDenominator;
+       return answer;
+    }else
+      if(Whole1 > 0 && Whole2 == 0){
+      //when fraction1 has a whole numbers but fraction2 doesn't
+      //add them after in improper fraction
+       int firstNumerator1 = (denominator1 * numerator2) + (denominator2 * removeWhole1); //removeWhole1 is the improperFraction of fraction1
+       int firstDenominator = denominator1 * denominator2;
+       String answer = firstNumerator1 + "/" + firstDenominator;
+       return answer;
+      }else
+        return "0";
+      }//end of additionDifferentNumerator method
 
 
-        // TODO: Implement this function to produce the solution to the input
-        // Checkpoint 1: Return the second operand.  Example "4/5 * 1_2/4" returns "1_2/4".
-        // Checkpoint 2: Return the second operand as a string representing each part.
-        //               Example "4/5 * 1_2/4" returns "whole:1 numerator:2 denominator:4".
-        // Checkpoint 3: Evaluate the formula and return the result as a fraction.
-        //               Example "4/5 * 1_2/4" returns "6/5".
-        //               Note: Answer does not need to be reduced, but it must be correct.
-        // Final project: All answers must be reduced.
-        //               Example "4/5 * 1_2/4" returns "1_1/5".
+   public static String additionSameNumerator(int numerator1, int numerator2, int Whole1, int Whole2, int denominator1, int denominator2, int removeWhole1, int removeWhole2){
+      //if denominators are the same
+       if(Whole1 > 0 && Whole2 > 0){//if whole number is greater than zero
+        int firstNumerator = removeWhole1 + removeWhole2; //removeWhole1 is the improperFraction of fraction1
+        //add the two fraction to  get the result
+          String answer = firstNumerator + "/" + denominator1;
+          return answer;
+      }else
+        if(Whole1 == 0 && Whole2 == 0){
+        int firstNumerator = numerator1 + numerator2; //when there is no whole numbers add the two numerator to get final numerator
+        //add the two fraction to get the result
+          String answer = (firstNumerator + "/" + denominator1); //results
+          return answer;
+      }else
+        if(Whole1 > 0 && Whole2 == 0){
+         int firstNumerator = removeWhole1 + numerator2; //removeWhole1 is the improperFraction of fraction1
+          String answer = (firstNumerator + "/" + denominator1);
+          return answer;
+      }else
+        if(Whole2 > 0 && Whole1 == 0){
+         int firstNumerator = removeWhole2 + numerator1; //removeWhole2 is the improperFraction of fraction2
+          String answer = (firstNumerator + "/" + denominator1);
+          return answer;
+      }
+       return "0";
+    }//end additionDifferentNumerator
 
 
-    // TODO: Fill in the space below with helper methods
 
-    /**
-     * greatestCommonDivisor - Find the largest integer that evenly divides two integers.
-     *      Use this helper method in the Final Checkpoint to reduce fractions.
-     * @param a - First integer.
-     * @param b - Second integer.
-     * @return The GCD.
-     */
-    /**public static int greatestCommonDivisor(int a, int b){
 
-      return 0;
-    }//end greatestCommonDivisor method
 
-    /**
-     * leastCommonMultiple - Find the smallest integer that can be evenly divided by two integers.
-     *      Use this helper method in Checkpoint 3 to evaluate expressions.
-     * @param a - First integer.
-     * @param b - Second integer.
-     * @return The LCM.
-     */
-    /**public static int leastCommonMultiple(int a, int b){
+   public static int improperFraction(int numerator1, int Whole1, int denominator1){
+        int removeWhole1 = (denominator1 * Whole1) + numerator1;
+        return removeWhole1;
 
-      //return 0;
-    }//end leastCommonMultiple
-    */
 
-//end class
+
+    }//end of improperFraction method
+}//end class
 
 
         // TODO: Implement this function to produce the solution to the input
