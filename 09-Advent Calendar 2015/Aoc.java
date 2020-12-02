33329cbed34fe90b3939fc0037f4fcd4f240fba2
XinBao321 <xinbaoc2@nycstudents.net>
2020-12-01 21:07:36 -0500

    fixed issues and now print out answer

--- 09-Advent Calendar 2015/Aoc.java
+++ 09-Advent Calendar 2015/Aoc.java
@@ -4,19 +4,22 @@ public class Aoc{
   public static void main(String[] args) {
 
     int answer = 0;
+    int result = 0;
+
     try{
-    Scanner input = new Scanner(new File("data.txt"));
+    Scanner input = new Scanner(new File("/Users/User/Desktop/javapro/fall-2020-work-XinBao321/09-Advent Calendar 2015/data.txt"));
 
       while(input.hasNext()){
         int number = Integer.parseInt(input.next());
-        int result = (number / 3) - 2;
+        result = (number / 3) - 2;
         answer += result;
+
     }//end while loop
 
       input.close();
   }//end try
 
-  catch(Exception e){
+  catch (Exception  e){
       System.out.println("File not found");
     }//end catch
       System.out.println(answer);
