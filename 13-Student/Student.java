3a6ea57b50e150e295654006e8f3d9448de1a607
xinbao321 <xinbaoc2@nycstudents.net>
2021-01-07 19:32:54 -0500

    What we did in class today :)

---
+++ 13-Student/Student.java
@@ -0,0 +1,32 @@
+public class Student(){
+ // instance varaibles
+ 
+ private String name;
+ private String osis;
+ private int[] grades;
+
+ // constructor
+
+
+ // other methods
+
+
+ public String getName(){
+
+ } //end of getName method
+
+
+ public String getOsis(){
+
+ } //end of getOsis method
+
+ public void setName(String name){
+
+ } //end of setName method
+
+ public double calculateAverage(){
+
+ } //end of calculateAverage method
+
+
+ }//end class
