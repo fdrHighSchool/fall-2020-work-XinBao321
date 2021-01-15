be10a72e39bb4bdaac6d7018be05d16112033f48
xinbao321 <xinbaoc2@nycstudents.net>
2021-01-14 19:27:52 -0500

    Added new work for food

--- 14-my_first_class/Person.java
+++ 14-my_first_class/Person.java
@@ -3,25 +3,26 @@ public class Person{
   // instances variables
   Scanner scan = new Scanner(System.in);
   private String favoriteFood;
-  private String age;
+  private int age;
   private String height;
   private String name;
   private String favoriteSport;
 
-  public Person( String n, String a, String h, String f, String fSport){
+  public Person( String n, int a, String h, String f, String fSport){
     this.name = n;
     this.age = a;
     this.height = h;
     this.favoriteFood = f;
     this.favoriteSport = fSport;
   } //end constructor
+  public void setName(String n){
+    this.name = n;
+  } //end setName method
 
-  public String getName(String name){
-
-    this.name = name;
+  public String getName(String n){
     System.out.println("Enter your name here: ");
-    String names = scan.nextLine();
-    return names;
+    n = scan.nextLine();
+    return  n;
   } //end of getName method
 
   public String getHeight(){
@@ -34,20 +35,22 @@ public class Person{
       double inches = scan.nextDouble();
 
       double height = inches * 2.54;
-      System.out.println("Your height is: " + height + " inches");
+      System.out.println("Your height is: " + height + " cm");
+
     } //end if statement
     if(choices == 2){
       System.out.println("Please enter your height in cm: ");
       double cm = scan.nextDouble();
 
       double height = cm / 2.54;
-      System.out.println("Your height is: " + height + " cm");
+      System.out.println("Your height is: " + height + " inches");
+
     } //end if statement
       System.out.println("");
-     return height;
+      return height;
   } //end getHeight method
 
-  public String getAge(){
+  public int getAge(){
 
     System.out.println("It's time to caclulate your age! ");
     System.out.println("Enter the year that you were born: ");
@@ -58,7 +61,9 @@ public class Person{
     int answer = yearRightNow - bornYear;
 
     System.out.println("Your age is: " + answer + " years old :D");
-    return "";
+    System.out.println("");
+
+    return answer;
   } //end getAge method
 
   public String getFavoriteFood(){
@@ -76,11 +81,64 @@ public class Person{
     System.out.println("11. Salmon");
     System.out.println("");
 
+
     int choices = scan.nextInt();
-    if(choices <= 12){
-      System.out.println("Thanks for sharing! , choices: " + choices + " is a great option :)");
-    }
-    return "";
+
+  switch(choices){
+    case 1:
+      if(choices == 1){
+        System.out.println(" The first pizza is thought to have been invented in Naples in the early 1500s. ");
+        break;
+      }
+
+    case 2:
+      if(choices == 2){
+        System.out.println("Bluefin Tuna Sushi is the costliest Sushi in the world.");
+        break;
+      }
+    case 3:
+      if(choices ==3 ){
+        System.out.println("Chicken is the most common type of poultry in the world. The largest serving of fried chicken (2,493 lbs) was served at Kentucky Fried Chicken");
+      }
+    case 4:
+      if(choices == 4){
+        System.out.println("The average Italian eats 60 pounds of pasta per year.");
+      }
+    case 5:
+      if(choices == 5){
+        System.out.println("Eggs are loaded with vitamins, minerals, high-quality protein, good fats and various other lesser-known nutrients");
+      }
+    case 6:
+        if(choices == 6){
+          System.out.println("The world's tallest ice cream cone was over 9 feet tall. It was scooped in Italy. Most of the vanilla used to make ice cream comes from Madagascar & Indonesia. ");
+      }
+    case 7:
+        if(choices == 7){
+          System.out.println("The word cereal comes from 'Ceres', the name of the Roman goddess of harvest and agriculture.");
+      }
+    case 8:
+        if(choices == 8){
+          System.out.println("Taco Bell is an American fast food chain known for its inventive, often whimsical, Mexican-inspired menu items");
+      }
+
+    case 9:
+        if(choices == 9){
+          System.out.println("Rice is the seed of the grass species Oryza sativa (Asian rice) or Oryza glaberrima (African rice). Rice is the oldest known food that is still widely consumed today.");
+      }
+    case 10:
+        if(choices == 10){
+          System.out.println("Although originated in China, the first instant ramen noodle was created in Japan and also the first kind of noodles to be consumed in space.");
+      }
+    case 11:
+        if(choices == 11){
+          System.out.println("Salmon is important part of human diet because it contains a lot of proteins, vitamin D and omega-3 fatty acids. Salmon do not eat any food during the time they swim upstream to spawn. ");
+      }
+    }// end switch statement;
+    System.out.println(" Thanks for sharing! , choices " + choices + " is a great option :)");
+    System.out.println("");
+
+
+    return choices;
   } // end getFavoriteFood methods
 
   public String getFavoriteSport(){
@@ -88,6 +146,6 @@ public class Person{
   } // end getFavoriteSport method
 
   public String toString() {
-    return "Person: " + this.name + " Age: " + this.age + " Height: " + this.height + " FavoriteFood: " + this.favoriteFood + " FavoriteSport: " + this.favoriteSport;
+    return "Person: " + this.name + " Age: " + this.age + " Height: " + height + " FavoriteFood: " + this.favoriteFood + " FavoriteSport: " + this.favoriteSport;
   } // end toString method
 } //end class
--- 14-my_first_class/PersonDriver.java
+++ 14-my_first_class/PersonDriver.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
 public class PersonDriver{
   public static void main(String[] args) {
-    Person FF1 = new Person("Myself","14","4 cm","Suhi","BasketBall");
-    Person FF2 = new Person("","","","","");
+    Person FF1 = new Person("Myself",14,"20 cm","Sushi","Basketball");
+    Person FF2 = new Person("",1,"21","","");
+    Scanner scan = new Scanner(System.in);
     System.out.println(FF1);
-    //FF1.getName();
+
     FF1.getHeight();
     FF1.getFavoriteFood();
     FF1.getAge();
-    System.out.println(FF2);
+    FF1.setName("XinBao");
+
+    System.out.println(FF1);
+
   } //end main method
+
 } //end class method
